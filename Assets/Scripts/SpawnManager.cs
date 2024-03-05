1c87bdbcb9a8bec7f8c94adfba65a4270a9bdd3a
Damla Metin Orak <damla91@gmail.com>
2024-03-06 00:15:52 +0300

    Triple shot powerup - 2
    - Added animation

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,30 +9,36 @@ public class SpawnManager : MonoBehaviour
     private GameObject _enemyPrefab;
     [SerializeField]
     private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _tripleShotPowerupPrefab;
 
     private bool _stopSpawning = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemyRoutine()
     {
-        
-    }
-
-    IEnumerator SpawnRoutine()
-    {
-        while(!_stopSpawning)
+        while (!_stopSpawning)
         {
             Vector3 posToSpawn = new Vector3(UnityEngine.Random.Range(-8f, 8f), 7f, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
-        }        
+        }
+    }
+
+    IEnumerator SpawnPowerupRoutine()
+    {
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(UnityEngine.Random.Range(-8f, 8f), 7f, 0);
+            Instantiate(_tripleShotPowerupPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(UnityEngine.Random.Range(3, 8));
+        }
     }
 
     public void OnPlayerDeath()
