6ffa2be8ee24ecd7a0d419072ebebf5d0fc55375
Damla Metin Orak <damla91@gmail.com>
2024-03-20 15:40:20 +0300

    FX - Asteroid Explosion

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 20.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
+    }
+}
